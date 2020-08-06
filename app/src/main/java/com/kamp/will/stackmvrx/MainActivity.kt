dc8839f0a336790216f4fccfc68d1929cfe3a916
William Kamp <manimaul@gmail.com>
2020-08-05 19:09:13 -0700

    init project

---
+++ app/src/main/java/com/kamp/will/stackmvrx/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.kamp.will.stackmvrx
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.kamp.will.stackmvrx.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                    .replace(R.id.container, MainFragment.newInstance())
+                    .commitNow()
+        }
+    }
+}
---
+++ app/src/main/java/com/kamp/will/stackmvrx/ui/main/MainViewModel.kt
@@ -0,0 +1,7 @@
+package com.kamp.will.stackmvrx.ui.main
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
