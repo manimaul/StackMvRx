397ce823204a46006814d27b2a13460340c2c3c3
William Kamp <manimaul@gmail.com>
2020-08-05 19:45:26 -0700

    mvrx in MainFragment

--- app/src/main/java/com/kamp/will/stackmvrx/ui/main/MainFragment.kt
+++ app/src/main/java/com/kamp/will/stackmvrx/ui/main/MainFragment.kt
@@ -1,23 +1,34 @@
 package com.kamp.will.stackmvrx.ui.main
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
-import androidx.fragment.app.viewModels
+import com.airbnb.mvrx.*
+import com.kamp.will.stackmvrx.BuildConfig
 import com.kamp.will.stackmvrx.R
+import kotlinx.android.synthetic.main.main_fragment.*
 
-class MainFragment : Fragment() {
+data class MainState(val title: String = "Hello World") : MvRxState
+
+class MainViewModel(initialState: MainState) : BaseMvRxViewModel<MainState>(initialState, debugMode = BuildConfig.DEBUG) {
+    fun getMoreExcited() = setState { copy(title = "$title!") }
+}
+
+class MainFragment : BaseMvRxFragment(R.layout.main_fragment) {
 
     companion object {
         fun newInstance() = MainFragment()
     }
 
-    private val viewModel: MainViewModel by viewModels()
+    private val viewModel: MainViewModel by fragmentViewModel()
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View {
-        return inflater.inflate(R.layout.main_fragment, container, false)
+    override fun invalidate() = withState(viewModel) { state ->
+        message.text = state.title
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        message.setOnClickListener {
+            viewModel.getMoreExcited()
+        }
     }
 }
--- app/src/main/java/com/kamp/will/stackmvrx/ui/main/MainViewModel.kt
+++
@@ -1,7 +0,0 @@
-package com.kamp.will.stackmvrx.ui.main
-
-import androidx.lifecycle.ViewModel
-
-class MainViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
-}
